d87ad495aa06478ebcd32e80adebd7a39b856937
NeuroRulez <neuro_999@hotmail.com>
2021-06-25 15:19:59 +0200

    Add DB9MIX Support

--- tecmo.sv
+++ tecmo.sv
@@ -127,9 +127,20 @@ module emu
   // 1 - D-/TX
   // 2..6 - USR2..USR6
   // Set USER_OUT to 1 to read from USER_IN.
-  input   [6:0] USER_IN,
-  output  [6:0] USER_OUT
+  output	    USER_OSD,
+  output  [1:0] USER_MODE,
+  input   [7:0] USER_IN,
+  output  [7:0] USER_OUT
 );
+
+wire         CLK_JOY = CLK_50M;         //Assign clock between 40-50Mhz
+wire   [2:0] JOY_FLAG  = {status[30],status[31],status[29]}; //Assign 3 bits of status (31:29) o (63:61)
+wire         JOY_CLK, JOY_LOAD, JOY_SPLIT, JOY_MDSEL;
+wire   [5:0] JOY_MDIN  = JOY_FLAG[2] ? {USER_IN[6],USER_IN[3],USER_IN[5],USER_IN[7],USER_IN[1],USER_IN[2]} : '1;
+wire         JOY_DATA  = JOY_FLAG[1] ? USER_IN[5] : '1;
+assign       USER_OUT  = JOY_FLAG[2] ? {3'b111,JOY_SPLIT,3'b111,JOY_MDSEL} : JOY_FLAG[1] ? {6'b111111,JOY_CLK,JOY_LOAD} : '1;
+assign       USER_MODE = JOY_FLAG[2:1] ;
+assign       USER_OSD  = joydb_1[10] & joydb_1[6];
 
 assign VGA_F1 = 0;
 
@@ -145,7 +156,6 @@ assign VIDEO_ARY = status[1] ? 8'd9  : status[2] ? 8'd4 : 8'd3;
 
 assign SDRAM_CLK = clk_sdram;
 
-assign USER_OUT = '1;
 
 `include "build_id.v"
 localparam CONF_STR = {
@@ -154,6 +164,8 @@ localparam CONF_STR = {
   "O2,Orientation,Horz,Vert;",
   "O3,Flip Screen,Off,On;",
   "O46,Scandoubler Fx,None,HQ2x,CRT 25%,CRT 50%,CRT 75%;",
+  "OUV,UserIO Joystick,Off,DB9MD,DB15 ;",
+  "OT,UserIO Players, 1 Player,2 Players;",  
   "O7,Debug,Off,On;",
   "-;",
   "DIP;",
@@ -197,7 +209,41 @@ wire  [7:0] ioctl_index;
 
 wire [10:0] ps2_key;
 
-wire  [9:0] joystick_0, joystick_1;
+wire [31:0] joystick_0 = joydb_1ena ? {joydb_1[9],joydb_1[11]|(joydb_1[10]&joydb_1[5]),joydb_1[10],joydb_1[6:0]} : joystick_0_USB;
+wire [31:0] joystick_1 = joydb_2ena ? {joydb_1[9],joydb_2[11]|(joydb_2[10]&joydb_2[5]),joydb_2[10],joydb_2[6:0]} : joydb_1ena ? joystick_0_USB : joystick_1_USB;
+  
+wire [15:0] joydb_1 = JOY_FLAG[2] ? JOYDB9MD_1 : JOY_FLAG[1] ? JOYDB15_1 : '0;
+wire [15:0] joydb_2 = JOY_FLAG[2] ? JOYDB9MD_2 : JOY_FLAG[1] ? JOYDB15_2 : '0;
+wire        joydb_1ena = |JOY_FLAG[2:1]              ;
+wire        joydb_2ena = |JOY_FLAG[2:1] & JOY_FLAG[0];
+
+//----BA 9876543210
+//----MS ZYXCBAUDLR
+reg [15:0] JOYDB9MD_1,JOYDB9MD_2;
+joy_db9md joy_db9md
+(
+  .clk       ( CLK_JOY    ), //40-50MHz
+  .joy_split ( JOY_SPLIT  ),
+  .joy_mdsel ( JOY_MDSEL  ),
+  .joy_in    ( JOY_MDIN   ),
+  .joystick1 ( JOYDB9MD_1 ),
+  .joystick2 ( JOYDB9MD_2 )	  
+);
+
+//----BA 9876543210
+//----LS FEDCBAUDLR
+reg [15:0] JOYDB15_1,JOYDB15_2;
+joy_db15 joy_db15
+(
+  .clk       ( CLK_JOY   ), //48MHz
+  .JOY_CLK   ( JOY_CLK   ),
+  .JOY_DATA  ( JOY_DATA  ),
+  .JOY_LOAD  ( JOY_LOAD  ),
+  .joystick1 ( JOYDB15_1 ),
+  .joystick2 ( JOYDB15_2 )	  
+);
+
+wire  [9:0] joystick_0_USB, joystick_1_USB;
 
 hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 (
@@ -219,8 +265,9 @@ hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
   .ioctl_download(ioctl_download),
   .ioctl_index(ioctl_index),
 
-  .joystick_0(joystick_0),
-  .joystick_1(joystick_1),
+  .joy_raw(joydb_1[5:0] | joydb_2[5:0]),
+  .joystick_0(joystick_0_USB),
+  .joystick_1(joystick_1_USB),
 
   .ps2_key(ps2_key)
 );
